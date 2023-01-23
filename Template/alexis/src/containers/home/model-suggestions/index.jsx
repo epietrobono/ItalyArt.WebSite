de2131f57d611275bd63a566ba61703d0ef08902
Luca <pietrobono50@gmail.com>
2023-01-24 00:14:57 +0100

    next

--- Template/alexis/src/containers/home/model-suggestions/index.jsx
+++ Template/alexis/src/containers/home/model-suggestions/index.jsx
@@ -4,14 +4,14 @@ import ModelSuggestionsData from "../../../data/model-suggestions.json";
 const ModelSuggestionsContainer = () => {
     const data = ModelSuggestionsData.it;
     return (
-        <div className="py-4 d-flex flex-column section-margin">
+        <div className="py-4 d-flex flex-column section-margin ">
             <h2 className="text-center">{data.title}</h2>
             <p className="text-center">{data.desc}</p>
             <div className="row justify-content-center">
                 {
                     data.suggestions.map((val, key)=>{
                         return(
-                            <div className="col col-auto" key={key}>
+                            <div className="col col-auto width-3d-card" key={key}>
                                 <MonumentCard data={val}></MonumentCard>
                             </div>
                         );
