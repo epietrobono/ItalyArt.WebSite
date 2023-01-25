5b46c5fee192c8b39e4a98548b6ed3b99bc43b2c
Luca <pietrobono50@gmail.com>
2023-01-25 13:56:36 +0100

    next

--- Template/alexis/src/containers/home/categories/index.jsx
+++ Template/alexis/src/containers/home/categories/index.jsx
@@ -3,7 +3,7 @@ import CategoriesData from "../../../data/categories.json";
 
 const CategoriesContainer = () => {
     return (
-    <div className="d-flex flex-column section-margin">
+    <div className="d-flex flex-column section-margin text-categorie-home">
         <h2 className="text-center">{CategoriesData.it.title}</h2>
         <span className="text-center">{CategoriesData.it.desc}</span>
         <CategoriesGallery data={CategoriesData.it.categories}></CategoriesGallery>
