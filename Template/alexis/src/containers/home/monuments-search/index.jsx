5edd1e5d2d44480006dcb16a37b699a267f7a078
Emanuele Pietrobono <epietrobono91@outlook.com>
2023-01-28 12:35:34 +0100

    fix

--- Template/alexis/src/containers/home/monuments-search/index.jsx
+++ Template/alexis/src/containers/home/monuments-search/index.jsx
@@ -7,32 +7,34 @@ const MonumentsSearchContainer = () => {
     const data = HomeMonumentsData.it;
     const banners= data.banners;
     return (
-        <Carousel className="home-banners" indicators={false}>
-                {
-                    data.banners.map((banner, key) => {
-                        return (
-                            <Carousel.Item style={{width: "100%"}} key={key}>
-                                <div
-                                    className="row monuments-search align-content-center justify-content-center  home-initial"
-                                    style={{
-                                        backgroundImage: `url(${
-                                            process.env.PUBLIC_URL + banner.backgroundImage
-                                        })`,
-                                    }}>
-                                        <div className="home-overlay"></div>  
-                                        <div className="home-bar monuments-form">
-                                            <h1 className="title text-center">{banner.title}</h1>
-                                            <div className="bg-light rounded barra-ricerca py-2 px-4">
-                                                <SearchForm data={banner} />
-                                            </div>
-                                        </div>
-                                    
-                                </div>
-                            </Carousel.Item>
-                        )
-                    })
-                }
-            </Carousel>
+        <>
+            <Carousel className="home-banners" indicators={false}>
+                    {
+                        data.banners.map((banner, key) => {
+                            return (
+                                <Carousel.Item style={{width: "100%"}} key={key}>
+                                    <div
+                                        className="row monuments-search align-content-center justify-content-center  home-initial"
+                                        style={{
+                                            backgroundImage: `url(${
+                                                process.env.PUBLIC_URL + banner.backgroundImage
+                                            })`,
+                                        }}>
+                                        
+                                    </div>
+                                </Carousel.Item>
+                            )
+                        })
+                    }
+                </Carousel>
+                <div className="home-overlay"></div>  
+                <div className="home-bar monuments-form">
+                    <h1 className="title text-center">{data.title}</h1>
+                    <div className="bg-light rounded barra-ricerca py-2 px-4">
+                        <SearchForm data={data} />
+                    </div>
+                </div>
+            </>
     )
     // return (
     //     <div>
