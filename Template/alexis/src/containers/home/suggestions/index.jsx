638b7c0ac517b838979e75e0c50e99f0304600ad
Luca <pietrobono50@gmail.com>
2023-04-06 12:58:16 +0200

    fix error

--- Template/alexis/src/containers/home/suggestions/index.jsx
+++ Template/alexis/src/containers/home/suggestions/index.jsx
@@ -21,10 +21,13 @@ const SuggestionsContainer = () => {
     // useEffect(() => {
     //     console.log(SuggestionsContainerData)
     // }, [SuggestionsContainerData]);
-    if(isLoading){
+    if (isLoading) {
         return null;
     }
-    const classColAuto= SuggestionsContainerData.Suggestions?.length==1 ? "col-auto" : "col-auto-in";
+    const classColAuto =
+        SuggestionsContainerData.Suggestions?.length == 1
+            ? "col-auto"
+            : "col-auto-in";
 
     return (
         <div className="container-ita-2">
@@ -41,8 +44,13 @@ const SuggestionsContainer = () => {
                             {SuggestionsContainerData.Suggestions.map(
                                 (val, key) => {
                                     return (
-                                        <div className={`col ${classColAuto} card-monumenti`} key={key}>
-                                            <MonumentCard data={val}></MonumentCard>
+                                        <div
+                                            className={`col ${classColAuto} `}
+                                            key={key}
+                                        >
+                                            <MonumentCard
+                                                data={val}
+                                            ></MonumentCard>
                                         </div>
                                     );
                                 }
