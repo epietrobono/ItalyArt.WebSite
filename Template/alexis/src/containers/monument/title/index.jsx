38c02f2a8449db38e9445284e2f362396a983f2a
Emanuele Pietrobono <epietrobono91@outlook.com>
2023-04-07 20:25:43 +0200

    Aggiunta Sottotitolo a Titolo

--- Template/alexis/src/containers/monument/title/index.jsx
+++ Template/alexis/src/containers/monument/title/index.jsx
@@ -5,7 +5,8 @@ const MonumentTitleContainer = ({contenuto}) => {
         <>
             <div className="row justify-content-center main-content container-ita-2">
                 <div className="col col-auto">
-                    <h2 className="title-monument">{contenuto.Titolo}</h2>
+                    <h2 className="title-monument text-center">{contenuto.Titolo}</h2>
+                    <h5 className="text-center">{contenuto.Testo}</h5>
                 </div>
             </div>
         </>
